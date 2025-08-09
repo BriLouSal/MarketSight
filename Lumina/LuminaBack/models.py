02d1ceb93d5edc5824000ce8f296771722faf22d
Brian Louis <brianlouis.salinas116@gmail.com>
2025-08-08 22:40:56 -0600

    Fixed contact us

--- Lumina/LuminaBack/models.py
+++ Lumina/LuminaBack/models.py
@@ -5,8 +5,16 @@ from django.db import models
 #  Creating Users 
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
-    pfp = models.ImageField()
     email = models.EmailField(unique=True)
+
+
+
+class Stock_Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock_ticker = models.CharField(max_length=10)
+    quantity = models.IntegerField()
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField()
 
 
     
